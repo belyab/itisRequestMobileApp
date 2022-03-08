3fde48acd355e917a725adbc047f492c90dab25b
Elmira <bbelyab@gmail.com>
2022-03-08 23:15:56 +0300

    some firebase fix

--- ITISRequest/ITISRequest/AppDelegate.swift
+++ ITISRequest/ITISRequest/AppDelegate.swift
@@ -9,10 +9,10 @@ import UIKit
 import CoreData
 import Firebase
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
